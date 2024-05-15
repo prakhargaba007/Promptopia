4752cc18ae22846b07ffdeab19ee545bf60344c7
prakhargaba007 <prakhargaba@gmail.com>
2024-05-15 21:53:45 +0530

    2nd commit

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -27,7 +27,11 @@ const Feed = () => {
   const [searchedResults, setSearchedResults] = useState([]);
 
   const fetchPosts = async () => {
-    const response = await fetch("/api/prompt", { cache: "no-store" });
+    const response = await fetch(
+      "/api/prompt",
+      { next: { revalidate: 10 } },
+      { cache: "no-store" }
+    );
     const data = await response.json();
 
     setAllPosts(data);
